70d58d9c14fe89abb7e9cd70e7a3b7be35552003
unchil <yeopo77@icloud.com>
2026-07-11 11:27:32 +0900

    Initial commit

--- composeApp/src/jvmMain/kotlin/com/unchil/oceanwaterinfo/SDoTEnvInfoUnionMapHexagonLayer.kt
+++ composeApp/src/jvmMain/kotlin/com/unchil/oceanwaterinfo/SDoTEnvInfoUnionMapHexagonLayer.kt
@@ -2,41 +2,24 @@ package com.unchil.oceanwaterinfo
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.copy
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowCircleRight
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldColors
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -49,22 +32,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.multiplatform.webview.web.LoadingState
 import com.multiplatform.webview.web.WebView
 import com.multiplatform.webview.web.rememberWebViewNavigator
 import com.multiplatform.webview.web.rememberWebViewState
-import com.unchil.oceanwaterinfo.AirQualityManager.airQualityStageComment
-import com.unchil.oceanwaterinfo.AirQualityManager.airQualityStageRange
-import com.unchil.oceanwaterinfo.AirQualityManager.information
 import com.unchil.oceanwaterinfo.AirQualityManager.nameEn
-import com.unchil.oceanwaterinfo.AirQualityManager.specialFeature
-import com.unchil.oceanwaterinfo.AirQualityManager.unHealthyForSensitiveGroups
 import com.unchil.oceanwaterinfo.viewmodel.SDoTEnvInfoUnionViewModel
 import kotlinx.coroutines.delay
-import javax.swing.tree.DefaultTreeCellEditor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -76,7 +52,7 @@ fun SDoTEnvInfoUnionMapHexagonLayer(
     val coroutineScope = rememberCoroutineScope()
 
     val viewModel: SDoTEnvInfoUnionViewModel = remember {
-        SDoTEnvInfoUnionViewModel(coroutineScope)
+        SDoTEnvInfoUnionViewModel()
     }
     LaunchedEffect(key1 = viewModel){
         while(true){
