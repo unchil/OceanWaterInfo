3e5c1b5e03316fd41ec8da5f9c075592b9eab79b
unchil <yeopo77@icloud.com>
2026-09-15 13:12:36 +0900

    Initial commit

--- composeApp/src/commonMain/kotlin/com/unchil/oceanwaterinfo/MobilePlatformView.kt
+++ composeApp/src/commonMain/kotlin/com/unchil/oceanwaterinfo/MobilePlatformView.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-val ChartBottonBarHeight = 120.dp
+val ChartBottonBarHeight = 100.dp
 
 val OceanWaterInfoDataGrid = object : SimpleChartView {
     override val name: String = "OceanWaterInfoDataGrid"
